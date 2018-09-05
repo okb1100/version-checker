543df6caa4332b0824b1eba1ed99efb5afa8d3a2
okb1100 <o.kucukbayrak@gmail.com>
2018-09-05 12:37:16 +0300

    First commit

---
+++ index.js
@@ -0,0 +1,18 @@
+// document.querySelectorAll('.htlgb')[6].firstChild.innerText
+
+const express = require('express');
+const request = require('request');
+const cheerio = require('cheerio');
+const app = express();
+const port = process.env.PORT || 3000;
+const playUrl = 'https://play.google.com/store/apps/details?id='
+const userAgent = 'Mozilla/5.0 (Linux; Android 5.0; SM-G900P Build/LRX21T) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.87 Mobile Safari/537.36'
+app.get('/:packageName', (req,res) => {
+	request({url: playUrl + req.params.packageName, headers: {'User-Agent': userAgent}}, (e, r, body) => {
+		const $ = cheerio.load(body)
+		const version = $('.htlgb').eq(6).children().first().text()
+		res.send(version === 'Varies with device' ? '0.0.0' : version)
+	})
+});
+
+app.listen(port, () => console.log('Listening on :' + port));
